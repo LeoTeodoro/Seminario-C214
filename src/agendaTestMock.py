5117df5a86d6d6434fbfd14eca0c94694daa4a91
Nicholas Siécola <n.siecola@gec.inatel.br>
2024-05-30 14:43:33 -0300

    Add teste "test_init"

---
+++ src/agendaTestMock.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.mock import MagicMock
+
+from Agenda import Agenda, Show
+
+class AgendaTestMock(unittest.TestCase):
+
+    def test_init(self):
+        agenda = Agenda()
+        self.assertEqual([], agenda.readShows())
+    
+if __name__ == '__main__':
+    unittest.main()
