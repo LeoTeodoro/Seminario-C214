f3646f9b138103afba8a35a1aff652420cb56498
Nicholas Siécola <n.siecola@gec.inatel.br>
2024-05-30 22:25:53 -0300

    Add test_updateShow_with_mock

--- src/tests/test_agendaMock.py
+++ src/tests/test_agendaMock.py
@@ -1,8 +1,7 @@
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, Mock
 
 from agenda import Agenda, Show
-
 
 class AgendaTestMock(unittest.TestCase):
 
@@ -16,22 +15,30 @@ class AgendaTestMock(unittest.TestCase):
         agenda.createShow(show_mock)
         self.assertEqual([show_mock], agenda.readShows())
 
-    def test_updateShow(self):
-        agenda = Agenda()
-        show_mock = MagicMock(spec=Show)
-        agenda.createShow(show_mock)
-        updated_show_mock = MagicMock(spec=Show)
-        agenda.updateShow(0, updated_show_mock)
-        self.assertEqual([updated_show_mock], agenda.readShows())
-
     @patch('agenda.Agenda.updateShow')
-    def test_updateShow_patch(self, mock_updateShow):
+    def test_updateShow_with_patch(self, mock_updateShow):
         agenda = Agenda()
         show_mock = MagicMock(spec=Show)
         agenda.createShow(show_mock)
         updated_show_mock = MagicMock(spec=Show)
         agenda.updateShow(0, updated_show_mock)
         mock_updateShow.assert_called_with(0, updated_show_mock)
+
+    def test_updateShow_with_Mock(self):
+        agenda_mock = Mock(spec=Agenda)
+        show_mock = Mock(spec=Show)
+        agenda_mock.createShow(show_mock)
+        updated_show_mock = Mock(spec=Show)
+        agenda_mock.updateShow(0, updated_show_mock)
+        agenda_mock.updateShow.assert_called_once_with(0, updated_show_mock)
+
+    def test_updateShow_with_MagicMock(self):
+        agenda = Agenda()
+        show_mock = MagicMock(spec=Show)
+        agenda.createShow(show_mock)
+        updated_show_mock = MagicMock(spec=Show)
+        agenda.updateShow(0, updated_show_mock)
+        self.assertEqual([updated_show_mock], agenda.readShows())
 
 if __name__ == '__main__':
     unittest.main()
